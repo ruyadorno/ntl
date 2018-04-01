13c68eddf8b29e5c1584520eb69072840d5c0fae
Ruy Adorno <ruyadorno@hotmail.com>
2018-04-01 07:43:23 -0400

    v2.0 refactor, fixes #12

---
+++ README.md
@@ -0,0 +1,87 @@
+# ntl
+
+[![NPM version](https://badge.fury.io/js/ntl.svg)](https://npmjs.org/package/ntl)
+[![Build Status](https://travis-ci.org/ruyadorno/ntl.svg?branch=master)](https://travis-ci.org/ruyadorno/ntl)
+[![License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)](https://raw.githubusercontent.com/ruyadorno/ipt/master/LICENSE)
+[![Join the chat at https://gitter.im/ipipeto/Lobby](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/ipipeto/Lobby)
+
+> Npm Task List
+
+Interactive cli menu to list and run npm tasks.
+
+![demo gif](http://i.imgur.com/ZjjQ7Vi.gif?1)
+
+> An [iPipeTo](https://github.com/ruyadorno/ipt) workflow
+
+<br />
+
+## Install
+
+```
+$ npm install -g ntl
+```
+
+**OR**
+
+just run it at once using **npx**:
+
+```sh
+npx ntl
+```
+
+<br />
+
+## Usage
+
+**cd** to a folder containing a `package.json` files that has configured **scripts**, then:
+
+```sh
+ntl
+```
+
+You can also specify a project folder containing a `package.json` file:
+
+```sh
+ntl ./my-node-project
+```
+
+<br />
+
+## More info
+
+cli options can also be invoked as their shorter alias:
+
+- `-a` -> `--all`
+- `-m` -> `--multiple`
+- `-m` -> `--size`
+- `-i` -> `--info`
+- `-h` -> `--help`
+- `-v` -> `--version`
+
+Here is what the help page looks like:
+
+```sh
+ntl --help
+
+Usage:
+  ntl [<path>]
+
+Options:
+  -a, --all           Includes pre and post scripts on the list        [boolean]
+  -A, --autocomplete  Starts in autocomplete mode                      [boolean]
+  -d, --debug         Prints to stderr any internal error              [boolean]
+  -h, --help          Shows this help message                          [boolean]
+  -i, --info          Displays the contents of each script             [boolean]
+  -m, --multiple      Allows the selection of multiple items           [boolean]
+  -s, --size          Amount of lines to display at once                [number]
+  -v, --version       Show version number                              [boolean]
+
+Visit https://github.com/ruyadorno/ntl for more info
+```
+
+<br />
+
+## License
+
+[MIT](LICENSE) © 2018 [Ruy Adorno](http://ruyadorno.com)
+
--- test/index.js
+++ test/index.js
@@ -1,20 +1,19 @@
-require('babel-core/register')({
-	ignore: function (filename) {
-		return !(filename.indexOf('tempdir/index.js') > -1);
+require("babel-core/register")({
+	ignore: function(filename) {
+		return !(filename.indexOf("tempdir/index.js") > -1);
 	}
 });
 
-var spawn = require('child_process').spawn;
-var path = require('path');
-var fs = require('fs');
-var test = require('ava').test;
-var tempdir = require('tempdir');
-var tempfile = require('tempfile');
-var ntl = require('../');
-var pkg = require('../package');
+var spawn = require("child_process").spawn;
+var path = require("path");
+var fs = require("fs");
+var test = require("ava").test;
+var tempdir = require("tempdir");
+var tempfile = require("tempfile");
+var pkg = require("../package");
 
 // Mocked deps
-var noop = function () {};
+var noop = function() {};
 var obj = Object.freeze({});
 var exec = noop;
 var log = {
@@ -34,7 +33,7 @@ var options = obj;
 
 test.beforeEach(function beforeEachTest(t) {
 	var stdin = tempfile();
-	fs.writeFileSync(stdin, '');
+	fs.writeFileSync(stdin, "");
 	t.context.p = {
 		stdin: fs.createReadStream(stdin),
 		stdout: fs.createWriteStream(tempfile()),
@@ -46,211 +45,108 @@ test.afterEach(function afterEachTest(t) {
 	t.context.p = null;
 });
 
-test.cb(function shouldDisplayWelcomeTitle(t) {
-	var testLog = {
-		info: function (msg) {
-			t.is(msg, 'Npm Task List - v' + pkg.version);
-			t.end();
-		}
-	};
-	ntl(t.context.p, exec, testLog, cwd, tasks, options);
-});
-
-test.cb(function shouldDisplayVersion(t) {
-	var testLog = {
-		info: function (msg) {
-			t.is(msg, pkg.version.toString());
-			t.end();
-		}
-	};
-	ntl(t.context.p, exec, testLog, cwd, tasks, {version: true});
-});
-
-test.cb(function shouldDisplayHelp(t) {
-	var testLog = {
-		info: function (msg) {
-			t.is(msg, pkg.version.toString());
-			t.end();
-		}
-	};
-	ntl(t.context.p, exec, testLog, cwd, tasks, {version: true});
-});
-
-test.cb(function shouldSelectDefaultTask(t) {
-	var testExec = function (name) {
-		t.is(name, 'npm run start');
-		t.end();
-	};
-	var prompt = ntl(t.context.p, testExec, log, cwd, tasks, options);
-	prompt.rl.emit('line');
-});
-
-test.cb(function shouldSelectTask(t) {
-	var testExec = function (name) {
-		t.is(name, 'npm run build');
-		t.end();
-	};
-	var prompt = ntl(t.context.p, testExec, log, cwd, tasks, options);
-	prompt.rl.input.emit('keypress', null, {name: 'down'});
-	prompt.rl.input.emit('keypress', null, {name: 'down'});
-	prompt.rl.emit('line');
-});
-
-test.cb(function shouldSelectPrefixedTasksWithAllFlag(t) {
-	var testExec = function (name) {
-		t.is(name, 'npm run pretest');
-		t.end();
-	};
-	var prompt = ntl(t.context.p, testExec, log, cwd, tasks, {all: true});
-	prompt.rl.input.emit('keypress', null, {name: 'up'});
-	prompt.rl.emit('line');
-});
-
-test.cb(function shouldDisplayScriptContentsWithInfoFlag(t) {
-	var testExec = function (name) {
-		t.is(name, 'npm run test');
-		t.end();
-	};
-	var prompt = ntl(t.context.p, testExec, log, cwd, tasks, {info: true});
-	prompt.rl.input.emit('keypress', null, {name: 'up'});
-	prompt.rl.emit('line');
-});
-
-test.cb(function shouldSelectMultipleTasksUsingFlag(t) {
-	var count = 0;
-	var testExec = function (name) {
-		t.ok(name.split(' ')[2] in {start: 1, debug: 1});
-		if (++count > 1) {
-			t.end();
-		}
-	};
-	var prompt = ntl(t.context.p, testExec, log, cwd, tasks, {multiple: true});
-	prompt.rl.input.emit('keypress', ' ', {name: 'space'});
-	prompt.rl.input.emit('keypress', null, {name: 'down'});
-	prompt.rl.input.emit('keypress', ' ', {name: 'space'});
-	prompt.rl.emit('line');
-});
-
-test.cb(function shouldNotFailOnNoTasksAvailable(t) {
-	var testLog = {
-		info: function (msg) {
-			t.is(msg, 'There are no npm scripts available here');
-			t.end();
-		}
-	};
-	ntl(t.context.p, exec, testLog, cwd, obj, options);
-});
-
 // --- cli integration tests
 
 test.cb(function shouldWorkFromCli(t) {
-	var content = '';
-	var run = spawn('node', ['../../cli.js'], {
-		cwd: path.join(__dirname, '/fixtures')
+	var content = "";
+	var run = spawn("node", ["../../cli.js"], {
+		cwd: path.join(__dirname, "/fixtures")
 	});
-	run.stdout.on('data', function (data) {
+	run.stdout.on("data", function(data) {
 		content += data.toString();
 	});
-	run.stderr.on('data', function (data) {
+	run.stderr.on("data", function(data) {
 		console.error(data.toString());
 	});
-	run.on('close', function (code) {
+	run.on("close", function(code) {
 		if (code !== 0) {
 			t.fail();
 		}
-		var values = content.split('\n');
-		t.is('build', values[values.length - 2]);
+		var values = content.split("\n");
+		t.is("build", values[values.length - 2]);
 		t.end();
 	});
-	run.stdin.write('\n');
+	run.stdin.write("\n");
 	run.stdin.end();
 });
 
 test.cb(function shouldWorkFromCliWithPath(t) {
-	var content = '';
-	var run = spawn('node', ['../cli.js', './fixtures'], {
+	var content = "";
+	var run = spawn("node", ["../cli.js", "./fixtures"], {
 		cwd: cwd
 	});
-	run.stdout.on('data', function (data) {
+	run.stdout.on("data", function(data) {
 		content += data.toString();
 	});
-	run.stderr.on('data', function (data) {
+	run.stderr.on("data", function(data) {
 		console.error(data.toString());
 	});
-	run.on('close', function (code) {
+	run.on("close", function(code) {
 		if (code !== 0) {
 			t.fail();
 		}
-		var values = content.split('\n');
-		t.is('build', values[values.length - 2]);
+		var values = content.split("\n");
+		t.is("build", values[values.length - 2]);
 		t.end();
 	});
-	run.stdin.write('\n');
+	run.stdin.write("\n");
 	run.stdin.end();
 });
 
 test.cb(function shouldWorkFromCliWithParams(t) {
-	var content = '';
-	var run = spawn('node', ['../cli.js', './fixtures', '--all', '-m'], {
+	var content = "";
+	var run = spawn("node", ["../cli.js", "./fixtures", "--all", "-m"], {
 		cwd: cwd
 	});
-	run.stdout.on('data', function (data) {
+	run.stdout.on("data", function(data) {
 		content += data.toString();
 	});
-	run.stderr.on('data', function (data) {
+	run.stderr.on("data", function(data) {
 		console.error(data.toString());
 	});
-	run.on('close', function (code) {
+	run.on("close", function(code) {
 		if (code !== 0) {
 			t.fail();
 		}
-		var values = content.split('\n');
-		t.is('prestart', values[values.length - 2]);
+		var values = content.split("\n");
+		t.is("prestart", values[values.length - 2]);
 		t.end();
 	});
-	run.stdin.write('j');
-	run.stdin.write('j');
-	run.stdin.write(' ');
-	run.stdin.write('\n');
+	run.stdin.write("j");
+	run.stdin.write("j");
+	run.stdin.write(" ");
+	run.stdin.write("\n");
 	run.stdin.end();
 });
 
 test.cb(function shouldExitWithErrorCodeOnNoPackageJson(t) {
-	tempdir().then(function (foldername) {
-		var run = spawn('node', [path.join(__dirname, '..', 'cli.js')], {
-			cwd: foldername
+	tempdir()
+		.then(function(foldername) {
+			var run = spawn("node", [path.join(__dirname, "..", "cli.js")], {
+				cwd: foldername
+			});
+			run.on("close", function(code) {
+				t.is(code, 1);
+				t.end();
+			});
+		})
+		.catch(function(e) {
+			console.error(e);
 		});
-		run.on('close', function (code) {
-			t.is(code, 1);
-			t.end();
-		});
-	}).catch(function (e) {
-		console.error(e);
-	});
 });
 
 test.cb(function shouldExitWithErrorMsgOnNoPackageJson(t) {
-	tempdir().then(function (foldername) {
-		var run = spawn('node', [path.join(__dirname, '..', 'cli.js')], {
-			cwd: foldername
+	tempdir()
+		.then(function(foldername) {
+			var run = spawn("node", [path.join(__dirname, "..", "cli.js")], {
+				cwd: foldername
+			});
+			run.stderr.on("data", function(data) {
+				t.is(data.toString(), "✖  No package.json found\n");
+				t.end();
+			});
+		})
+		.catch(function(e) {
+			console.error(e);
 		});
-		run.stderr.on('data', function (data) {
-			t.is(data.toString(), 'No package.json found\n');
-			t.end();
-		});
-	}).catch(function (e) {
-		console.error(e);
-	});
-});
-
-test.cb(function shouldExitWithCodeZeroOnPressedESC(t) {
-	var run = spawn('node', ['../cli.js', './fixtures'], {
-		cwd: cwd
-	});
-	run.on('close', function (code) {
-		t.is(code, 0);
-		t.end();
-	});
-	run.stdin.write('\u001b');
-	run.stdin.end();
 });
