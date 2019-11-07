a8f2b21982a042b8bc7f0a5fa8d375ff7d836e6b
Ruy Adorno <ruyadorno@hotmail.com>
2019-11-07 09:34:15 -0500

    Add simple run integration test

---
+++ test/integration/simple-run.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const path = require("path");
+const fs = require("fs");
+
+const Minipass = require("minipass");
+const { test } = require("tap");
+const spawn = require("cross-spawn");
+
+test("ntl run and select first item", t => {
+	const cwd = t.testdir({
+		"package.json": JSON.stringify({
+			scripts: {
+				build: 'echo "build"'
+			}
+		})
+	});
+
+	const run = spawn("node", ["../../../cli.js"], { cwd });
+	run.stderr.on("data", data => {
+		console.error(data.toString());
+		t.fail("should not have stderr output");
+	});
+
+	const ministream = new Minipass();
+	run.stdout.pipe(ministream);
+	ministream.collect().then(res => {
+		const taskOutput = res[res.length - 1].toString().trim();
+		t.equal(taskOutput, "build", "should be able to run task");
+		t.end();
+	});
+
+	run.stdin.write("\n");
+	run.stdin.end();
+});
