90b3f8b13be1cf7487036a3ef35a241ea02b569f
Ruy Adorno <ruyadorno@hotmail.com>
2019-11-07 09:37:12 -0500

    Added simple unit test

---
+++ test/unit/simple-list.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const { Passthrough } = require("minipass");
+const { test } = require("tap");
+const requireInject = require("require-inject");
+
+test("build a simple list of items", t => {
+	const ntl = requireInject("../../cli", {
+		"read-pkg": {
+			sync: () => ({
+				scripts: {
+					test: "make test",
+					build: "make build"
+				}
+			})
+		},
+		ipt: expected => {
+			t.deepEqual(
+				expected,
+				["test", "build"],
+				"should build a list with the task names"
+			);
+			t.end();
+			return Promise.resolve([]);
+		}
+	});
+});
