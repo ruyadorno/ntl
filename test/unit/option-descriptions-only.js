ecd9999453baa9fa6581132cfec90ff872675a56
Ruy Adorno <ruyadorno@hotmail.com>
2019-11-11 23:57:49 -0500

    Added more tests for -o

--- test/unit/option-descriptions-only.js
+++ test/unit/option-descriptions-only.js
@@ -33,8 +33,55 @@ test("build a list using --descriptions-only option", t => {
 			t.end();
 			return Promise.resolve([]);
 		},
+		"simple-output": {
+			success: msg => {
+				t.ok(msg.startsWith("Npm Task List"), "should show success msg");
+			}
+		},
 		yargs: mockYargs({
 			_: [],
+			descriptionsOnly: true
+		})
+	});
+});
+
+test("build a list using --descriptions-only option along with --description", t => {
+	const ntl = requireInject("../../cli", {
+		"read-pkg": {
+			sync: () => ({
+				scripts: {
+					build: "make build",
+					test: "make test"
+				},
+				ntl: {
+					descriptions: {
+						build: "Run build steps"
+					}
+				}
+			})
+		},
+		ipt: expected => {
+			t.deepEqual(
+				expected,
+				[
+					{
+						name: "build › Run build steps",
+						value: "build"
+					}
+				],
+				"should build a list with the task names"
+			);
+			t.end();
+			return Promise.resolve([]);
+		},
+		"simple-output": {
+			success: msg => {
+				t.ok(msg.startsWith("Npm Task List"), "should show success msg");
+			}
+		},
+		yargs: mockYargs({
+			_: [],
+			descriptions: true,
 			descriptionsOnly: true
 		})
 	});
